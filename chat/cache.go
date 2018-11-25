cb091d0cadfe436226ceb66ae8cebaa832db1b8b
Alex Tan Hong Pin <alextan220990@gmail.com>
2018-11-26 01:10:14 +0800

    chore(cache): add comments

--- chat/cache.go
+++ chat/cache.go
@@ -20,35 +20,35 @@ func NewTableCache(client *redis.Client) *TableCache {
 // Add adds both user and room to the cache.
 func (t *TableCache) Add(user, room string) error {
 	pipe := t.client.Pipeline()
+	// Add room to user's list. 
 	pipe.SAdd(userKey(user), room)
+	// Add user to the room.
 	pipe.SAdd(roomKey(room), user)
-	log.Println("added", userKey(user), roomKey(room))
 	_, err := pipe.Exec()
 	return err
 }
 
+// Delete removes the user from the room, and clears the user's room list.
 func (t *TableCache) Delete(user string, fn func(string)) error {
-	log.Println("called delete pipeline", user)
-	pipe := t.client.Pipeline()
+	// Get the rooms the user is in.
 	rooms := t.client.SMembers(userKey(user)).Val()
-	// Get the rooms that the user belong to.
-	// rooms := pipe.SMembers(userKey(user)).Val()
-	log.Println("rooms belonging to", rooms, user)
-
+	
+	pipe := t.client.Pipeline()
 	// For each room, remove the user from the set.
 	for _, room := range rooms {
 		pipe.SRem(roomKey(room), user)
-		log.Println("table cache", room)
 		fn(room)
 	}
 	_, err := pipe.Exec()
 	return err
 }
 
+// GetRooms returns the rooms the user is in.
 func (t *TableCache) GetRooms(user string) []string {
 	return t.client.SMembers(userKey(user)).Val()
 }
 
+// GetUsers returns the users in a room.
 func (t *TableCache) GetUsers(room string) []string {
 	return t.client.SMembers(roomKey(room)).Val()
 }
